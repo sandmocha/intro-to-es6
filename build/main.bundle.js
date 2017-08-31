b9ec401220dcf06b2051c0b3912c931d39be0934
Pakteema <sand_mocha@hotmail.com>
2017-08-31 20:11:14 +0700

    sand

---
+++ build/main.bundle.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function word(hi) {
+	document.getElementById("mocha").innerHTML = document.getElementById("mocha").innerHTML + hi;
+}
+function words() {
+	var y = document.getElementById("mocha").innerHTML;
+	var x = document.getElementById("mocha").innerHTML.length;
+	document.getElementById("mocha").innerHTML = y.substring(0, x - 1);
+}
+function cle() {
+	document.getElementById("mocha").innerHTML = ' ';
+}
+function bar(hi) {
+	document.getElementById("mocha").innerHTML = document.getElementById("mocha").innerHTML + ' ';
+}
